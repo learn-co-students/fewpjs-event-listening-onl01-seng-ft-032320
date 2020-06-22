77167e36a8a74422d6214af932080df805acde81
Swishyfishie2 <pclandreiuk@gmail.com>
2020-06-22 04:43:19 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+
+    const input = document.querySelector('#input')
+    console.log(input)
+    input.addEventListener("click", function(e) {
+        alert('I was clicked')
+        console.log(e)
+    })
+
+
 }
+
+addingEventListener()
